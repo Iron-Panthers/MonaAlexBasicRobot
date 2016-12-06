b1c09b7b63f0204d9130576b1a485a04638216b7
Alex Mak <alexanderajm@gmail.com>
2016-12-05 16:43:13 -0800

    New Proj

--- src/org/usfirst/frc/team5026/robot/Robot.java
+++ src/org/usfirst/frc/team5026/robot/Robot.java
@@ -5,8 +5,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team5026.robot.commands.ArcadeDriveWithJoystick;
-import org.usfirst.frc.team5026.robot.subsystems.Drive;
+import org.usfirst.frc.team5026.robot.commands.ExampleCommand;
+import org.usfirst.frc.team5026.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static final Drive exampleSubsystem = new Drive();
+	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
     Command autonomousCommand;
@@ -32,7 +32,7 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
 		oi = new OI();
         chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ArcadeDriveWithJoystick());
+        chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
     }
--- src/org/usfirst/frc/team5026/robot/commands/ExampleCommand.java
+++ src/org/usfirst/frc/team5026/robot/commands/ExampleCommand.java
@@ -8,22 +8,19 @@ import org.usfirst.frc.team5026.robot.Robot;
 /**
  *
  */
-public class ArcadeDriveWithJoystick extends Command {
+public class ExampleCommand extends Command {
 
-	
-    public ArcadeDriveWithJoystick() {
+    public ExampleCommand() {
         // Use requires() here to declare subsystem dependencies
         requires(Robot.exampleSubsystem);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,12 +30,10 @@ public class ArcadeDriveWithJoystick extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	
     }
 }
